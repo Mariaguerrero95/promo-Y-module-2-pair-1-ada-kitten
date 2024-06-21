d18093eb9ee213a4445262fcf69ea34eb7269271
Mariaguerrero95 <mariaguerrero1995@outlook.es>
2024-06-21 11:27:57 +0200

    js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
-'use strict';
-document.querySelector(".js-list");
-const card1 = <li class="card">
+"use strict";
+/* Agregar el código del li desde HTMl 
+Repetir este proceso por cada gatito */
+const kittenOne = `<li class="card">
 <article>
   <img
     class="card_img"
     src="https://dev.adalab.es/gato-siames.webp"
-    alt="siames-cat"/>
+    alt="gatito"
+  />
   <h3 class="card_title">Anastacio</h3>
   <h4 class="card_race">Siamés</h4>
   <p class="card_description">
-    Porte elegante, su patrón de color tan característico y sus ojos
-    de un azul intenso, pero su historia se remonta a Asía al menos
-    hace 500 años, donde tuvo su origen muy posiblemente.
-  </p>
+            Porte elegante, su patrón de color tan característico y sus ojos
+            de un azul intenso, pero su historia se remonta a Asía al menos
+            hace 500 años, donde tuvo su origen muy posiblemente.
+   </p>
 </article>
 </li>;
-
