668c2e7cca2b27a42af0b7ed3a908a781f47aea3
Olga <olgaalaquas@gmail.com>
2024-06-26 10:52:46 +0200

    changes in ejercice 1

--- js-adakitten/main.js
+++ js-adakitten/main.js
@@ -1,6 +1,16 @@
 "use strict";
 /* Agregar el código del li desde HTMl 
 Repetir este proceso por cada gatito */
+const description1 = `Porte elegante, su patrón de color tan característico y sus ojos
+de un azul intenso, pero su historia se remonta a Asía al menos
+hace 500 años, donde tuvo su origen muy posiblemente.`
+const description2 = `Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
+hasta con pinta de alienígena han llegado a definir a esta raza
+gatuna que se caracteriza por la «ausencia» de pelo.`
+const description3 = `Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
+bella mirada se ha convertido en una de sus señas de identidad.
+Sus ojos son grandes y las orejas resultan largas y en punta.`
+
 const kittenOne = `<li class="card">
 <article>
   <img
@@ -11,12 +21,39 @@ const kittenOne = `<li class="card">
   <h3 class="card_title">Anastacio</h3>
   <h4 class="card_race">Siamés</h4>
   <p class="card_description">
-            Porte elegante, su patrón de color tan característico y sus ojos
-            de un azul intenso, pero su historia se remonta a Asía al menos
-            hace 500 años, donde tuvo su origen muy posiblemente.
+  ${description1}          
   </p>
 </article>
 </li>;`
+
+const kittenTwo = `<li class="card">
+  <img
+  class="card_img"
+  src="https://dev.adalab.es/sphynx-gato.webp"
+  alt="sphynx-cat"
+  />
+  <h3 class="card_title">Fiona</h3>
+  <h4 class="card_race">Sphynx</h4>
+  <p class="card_description">
+  ${description2}          
+  </p>
+  </li>`
+
+  const kittenThree = `<li class="card">
+  <img
+    class="card_img"
+    src="https://dev.adalab.es/maine-coon-cat.webp"
+    alt="maine-coon-cat"
+  />
+  <h3 class="card_title">Cielo</h3>
+  <h4 class="card_race">Maine Coon</h4>
+  <p class="card_description js-description3">
+  ${description3}
+  </p>
+</li>`
+
+document.querySelector(".js-list")
+js-list.innerHTML 
 
 /* 
   1.Mostrar el formulario por defecto
@@ -42,19 +79,18 @@ const kitty3 = document.querySelector('.js-description3');
 button1.addEventListener("click", (event) => {
   event.preventDefault();
   const descrSearchText = input_search_desc.value;
-  if (kitty3 === )
-    if( kittenDesc1.includes(descrSearchText) ) {
-    //Completa el código
-    }
-    
-    if( kittenDesc2.includes(descrSearchText) ) {
+  console.log(descrSearchText)
+    if( description1.includes(descrSearchText) ) {
     
     }
     
-    if( kittenDesc3.includes(descrSearchText) ) {
+    if( description2.includes(descrSearchText) ) {
+    
+    }
+    
+    if( description3.includes(descrSearchText) ) {
     kitty3.innerHTML
     }
-    console.log("gatito")
 })
 
 
